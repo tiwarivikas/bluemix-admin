64d3c28c2da6c72df581e180057c01c3ebce9f27
Vikas Tiwari <tiwari.vikas@gmail.com>
2016-01-29 13:38:36 +0530

    Updated favicon image

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var app = express();
 //Express Configuration
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(favicon(path.join(__dirname, 'public/images', 'favicon.png')));
+app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
